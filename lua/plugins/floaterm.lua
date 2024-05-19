3ed39415b5094934e9f69d5a3e2b52bbf4442878
ntn2142 <ntn2142@proton.me>
2024-05-19 23:40:09 +0200

    floaterm keybinds

--- lua/plugins/floaterm.lua
+++ lua/plugins/floaterm.lua
@@ -1,4 +1,7 @@
 return {
 	"voldikss/vim-floaterm",
-	opts = {},
+	config = function()
+		vim.keymap.set("n", "<leader>tl", "<cmd>FloatermNew Lazygit<CR>", { desc = "[T]oggle [L]azygit" })
+		vim.keymap.set("n", "<leader>tt", "<cmd>FloatermNew<CR>", { desc = "[T]oggle [T]erminal" })
+	end,
 }
