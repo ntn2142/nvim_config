38c5f4ff5cbb916c8764cd5d4bdbb02e093c3bd2
ntn2142 <ntn2142@proton.me>
2024-07-15 17:48:32 +0200

    whick-key config rewrite

--- lua/plugins/floaterm.lua
+++ lua/plugins/floaterm.lua
@@ -1,7 +1,7 @@
 return {
 	"voldikss/vim-floaterm",
 	config = function()
-		vim.keymap.set("n", "<leader>fl", "<cmd>FloatermNew Lazygit<CR>", { desc = "[F]loat [L]azygit" })
+		vim.keymap.set("n", "<leader>fl", "<cmd>FloatermNew lazygit<CR>", { desc = "[F]loat [L]azygit" })
 		vim.keymap.set("n", "<leader>ft", "<cmd>FloatermNew<CR>", { desc = "[F]loat [T]erminal" })
 	end,
 }
